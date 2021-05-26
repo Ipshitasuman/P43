429e81bde1ba12201b0eb8dc3493443ab00f9245
Ipshitasuman <74243297+Ipshitasuman@users.noreply.github.com>
2021-05-26 11:48:01 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,17 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50);
+    text("I O T F N N U C", 700,130);
+    text("A T E D A S A B", 100,300);
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70);
+    text("Hint: Performs a particular task !!", 700,150);
+    text("Hint: Stores all information !!", 100,320);
+  
+}
